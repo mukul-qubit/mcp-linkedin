53b4ba4274dfd29ecaa8cc4e4a391a21684565d0
mukul-qubit <mukul.kathayat@qubit.capital>
2025-05-18 19:58:16 +0530

    Update linkedin_api_tools.py

--- linkedin_api_tools.py
+++ linkedin_api_tools.py
@@ -1227,4 +1227,5 @@ def person_data_with_educations(link: str) -> Dict:
     return json.loads(data.decode("utf-8"))
 
 if __name__ == "__main__":
+    mcp.start()
     mcp.run(transport="sse", host="0.0.0.0", port=80)
